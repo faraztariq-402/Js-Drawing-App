7ef49577f832827436502d6a22e3291adb6bedb5
Faraz Tariq <faraztariq12123@gmail.com>
2023-05-24 14:49:48 +0500

    js paint app

--- app.js
+++ app.js
@@ -4,109 +4,257 @@ let range = document.getElementById("range");
 let eraser = document.getElementById("eraser");
 let eraserRange = document.getElementById("eraserRange");
 let brush = document.getElementById("brush");
-let lineWidth = 10;
-let triangleShape = document.getElementById("triangleShape")
-let circlePic = document.getElementById("circlePic")
-let rectanglePic = document.getElementById("rectanglePic") 
-let isDrawing = false
+let rectanglePic = document.getElementById("rectanglePic");
+let circlePic = document.getElementById("circlePic");
+let triangleShape = document.getElementById("triangleShape");
+let currentMode = "brush"; // Default mode is brush
+let isDrawing = false;
+let finalOffsetX, finalOffsetY;
+
 window.addEventListener("load", () => {
-    canvas.width = 1600;
-    canvas.height = canvas.offsetHeight;
+  canvas.width = 1600;
+  canvas.height = 1000;
 });
 
 let selectColor = () => {
-    let userColor = document.getElementById("userInput").value;
-    context.strokeStyle = userColor || "#000000"; // set default stroke color to black
-    context.lineWidth = range.value;
+  let userColor = document.getElementById("userInput").value;
+  context.strokeStyle = userColor || "#000000"; // set default stroke color to black
+  context.lineWidth = range.value;
 };
 
-let switchToBrush = () => {
-    context.globalCompositeOperation = "source-over"; // Set brush mode
+let switchMode = (mode) => {
+  currentMode = mode;
+  if (mode === "brush") {
     context.lineWidth = range.value;
-};
-
-
-
-
-
-
-
-  
-
-let switchToEraser = () => {
-    context.globalCompositeOperation = "destination-out"; // Set eraser mode
+    context.globalCompositeOperation = "source-over";
+    context.lineCap = "round"; // Set lineCap to round for smooth brush strokes
+  } else if (mode === "eraser") {
+    context.globalCompositeOperation = "destination-out";
     context.lineWidth = eraserRange.value;
+    context.lineCap = "round"; // Set lineCap to round for smoother erasing
+  }
 };
+
+let switchToTriangle = () => {
+  currentMode = "triangle";
+};
+
+let switchToRectangle = () => {
+  currentMode = "rectangle";
+};
+
+let switchToCircle = () => {
+  currentMode = "circle";
+};
+
+canvas.addEventListener("mousedown", (event) => {
+  isDrawing = true;
+  finalOffsetX = event.offsetX;
+  finalOffsetY = event.offsetY;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (isDrawing) {
+    if (currentMode === "brush") {
+      context.lineTo(event.offsetX, event.offsetY);
+      context.stroke();
+    } else if (currentMode === "triangle") {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.beginPath();
+      context.moveTo(finalOffsetX, finalOffsetY); // Move to the initial point
+      context.lineTo(event.offsetX, event.offsetY); // Draw a line to the current mouse position
+      context.lineTo(
+        finalOffsetX + (finalOffsetX - event.offsetX),
+        event.offsetY
+      ); // Draw a line to the calculated third point
+      context.closePath(); // Close the path
+      context.stroke(); // Stroke the triangle outline
+    }else if(currentMode === "eraser" ){
+      context.lineWidth = eraserRange.value;
+
+      context.beginPath();
+      context.moveTo(finalOffsetX, finalOffsetY);
+      context.lineTo(event.offsetX, event.offsetY);
+      context.stroke();
+      finalOffsetX = event.offsetX;
+      finalOffsetY = event.offsetY;
+      context.globalCompositeOperation = "destination-out";
+      context.strokeStyle = board.style.backgroundColor;
+      context.lineCap = "round";// Draw a line to the calculated third point
+      context.closePath(); // Close the path
+      context.stroke(); // Stroke the triangle outline
+    }  else if (currentMode === "rectangle") {
+    
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      let width = event.offsetX - finalOffsetX;
+      let height = event.offsetY - finalOffsetY;
+      context.strokeRect(finalOffsetX, finalOffsetY, width, height);
+    } else if (currentMode === "circle") {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      let radius = Math.sqrt(
+        Math.pow(event.offsetX - finalOffsetX, 2) +
+          Math.pow(event.offsetY - finalOffsetY, 2)
+      );
+      context.beginPath();
+      context.arc(finalOffsetX, finalOffsetY, radius, 0, 2 * Math.PI);
+      context.stroke();
+    }
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener("mouseleave", () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener("touchstart", (event) => {
+  isDrawing = true;
+  let touch = event.touches[0];
+  finalOffsetX = touch.pageX - canvas.offsetLeft;
+  finalOffsetY = touch.pageY - canvas.offsetTop;
+});
+
+canvas.addEventListener("touchmove", (event) => {
+  if (isDrawing) {
+    event.preventDefault();
+    let touch = event.touches[0];
+    let offsetX = touch.pageX - canvas.offsetLeft;
+    let offsetY = touch.pageY - canvas.offsetTop;
+    if (currentMode === "brush") {
+      context.lineTo(offsetX, offsetY);
+      context.stroke();
+    }else if(currentMode === "eraser" ){
+      context.lineWidth = eraserRange.value;
+
+      context.beginPath();
+      context.moveTo(finalOffsetX, finalOffsetY);
+      context.lineTo(event.offsetX, event.offsetY);
+      context.stroke();
+      finalOffsetX = event.offsetX;
+      finalOffsetY = event.offsetY;
+      context.globalCompositeOperation = "destination-out";
+      context.lineCap = "round";
+    }
+     else if (currentMode === "triangle") {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.beginPath();
+      context.moveTo(finalOffsetX, finalOffsetY); // Move to the initial point
+      context.lineTo(offsetX, offsetY); // Draw a line to the current touch position
+      context.lineTo(finalOffsetX + (finalOffsetX - offsetX), offsetY); // Draw a line to the calculated third point
+      context.closePath(); // Close the path
+      context.stroke(); // Stroke the triangle outline
+    } else if (currentMode === "rectangle") {
+      
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.beginPath();
+      // context.moveTo(finalOffsetX, finalOffsetY); // Move to the initial point
+      // context.lineTo(offsetX, offsetY);
+      let width = offsetX - finalOffsetX;
+      let height = offsetY - finalOffsetY;
+      context.strokeRect(finalOffsetX, finalOffsetY, width, height);
+    } else if (currentMode === "circle") {
+      let radius = Math.sqrt(
+        Math.pow(offsetX - finalOffsetX,2) +
+        Math.pow(offsetY - finalOffsetY,2)
+      );
+    
+      // Clear the canvas
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    
+      // Redraw any existing elements on the canvas
+      // ...
+    
+      // Draw the circle
+      context.beginPath();
+      context.arc(finalOffsetX, finalOffsetY, radius, 0, 2 * Math.PI);
+      context.stroke();
+      }
+
+    
+  }
+});
+
+canvas.addEventListener("touchend", () => {
+  isDrawing = false;
+});
 
 let startDrawing = () => {
-     isDrawing = false;
-    let x = 0;
-    let y = 0;
+  let x = 0;
+  let y = 0;
 
-    canvas.addEventListener("mousedown", (event) => {
-        isDrawing = true;
-        x = event.offsetX;
-        y = event.offsetY;
-        context.beginPath();
-        context.moveTo(x, y);
-    });
+  canvas.addEventListener("mousedown", (event) => {
+    isDrawing = true;
+    x = event.offsetX;
+    y = event.offsetY;
+    context.beginPath();
+    context.moveTo(x, y);
+  });
 
-    canvas.addEventListener("mousemove", (event) => {
-        if (isDrawing) {
-            context.lineTo(event.offsetX, event.offsetY);
-            context.stroke();
-            x = event.offsetX;
-            y = event.offsetY;
-        }
-    });
+  canvas.addEventListener("mousemove", (event) => {
+    if (isDrawing && currentMode === "brush") {
+      context.lineTo(event.offsetX, event.offsetY);
+      context.stroke();
+      x = event.offsetX;
+      y = event.offsetY;
+    }
+  });
 
-    canvas.addEventListener("mouseup", () => {
-        isDrawing = false;
-    });
+  canvas.addEventListener("mouseup", () => {
+    isDrawing = false;
+  });
 
-    canvas.addEventListener("mouseleave", () => {
-        isDrawing = false;
-    });
+  canvas.addEventListener("mouseleave", () => {
+    isDrawing = false;
+  });
 
-    canvas.addEventListener("touchstart", (event) => {
-        let touch = event.touches[0];
-        x = touch.pageX - canvas.offsetLeft;
-        y = touch.pageY - canvas.offsetTop;
-        context.beginPath();
-        context.moveTo(x, y);
-    });
+  canvas.addEventListener("touchstart", (event) => {
+    let touch = event.touches[0];
+    x = touch.pageX - canvas.offsetLeft;
+    y = touch.pageY - canvas.offsetTop;
+    context.beginPath();
+    context.moveTo(x, y);
+  });
 
-    canvas.addEventListener("touchmove", (event) => {
-        event.preventDefault();
-        let touch = event.touches[0];
-        let offsetX = touch.pageX - canvas.offsetLeft;
-        let offsetY = touch.pageY - canvas.offsetTop;
-        context.lineTo(offsetX, offsetY);
-        context.stroke();
-        x = offsetX;
-        y = offsetY;
-    });
+  canvas.addEventListener("touchmove", (event) => {
+    event.preventDefault();
+    let touch = event.touches[0];
+    let offsetX = touch.pageX - canvas.offsetLeft;
+    let offsetY = touch.pageY - canvas.offsetTop;
+    context.lineTo(offsetX, offsetY);
+    context.stroke();
+    x = offsetX;
+    y = offsetY;
+  });
 };
 
 let clearBoard = (event) => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    event.preventDefault()
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  event.preventDefault();
 };
 
 let savePic = () => {
-    let urlData = canvas.toDataURL("image/png");
-    let link = document.createElement("a");
-    link.download = "Faraz.png";
-    link.href = urlData;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  let urlData = canvas.toDataURL("image/png");
+  let link = document.createElement("a");
+  link.download = "Faraz.png";
+  link.href = urlData;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 };
 
-document.getElementById("userInput").addEventListener("change", selectColor);
-document.getElementById("range").addEventListener("change", selectColor);
-
-eraser.addEventListener("click", switchToEraser);
-brush.addEventListener("click", switchToBrush);
-
 startDrawing();
+document.getElementById("userInput").addEventListener("change", selectColor);
+range.addEventListener("change", selectColor);
+brush.addEventListener("click", () => switchMode("brush"));
+eraser.addEventListener("click", () => switchMode("eraser"));
+rectanglePic.addEventListener("click", switchToRectangle);
+circlePic.addEventListener("click", switchToCircle);
+triangleShape.addEventListener("click", switchToTriangle);
+
+
+
+
+
